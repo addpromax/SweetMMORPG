f192aeb01453391c3ed094969ac917b83aa4a4ab
人間工作 <coolxiaom95@gmail.com>
2025-01-19 18:00:01 +0800

    fix class cast exception

--- src/main/java/top/mrxiaom/sweet/mmorpg/SweetMMORPG.java
+++ src/main/java/top/mrxiaom/sweet/mmorpg/SweetMMORPG.java
@@ -1,12 +1,18 @@
 package top.mrxiaom.sweet.mmorpg;
         
 import net.Indyuce.mmoitems.MMOItems;
+import net.Indyuce.mmoitems.comp.rpg.RPGHandler;
 import org.bukkit.Bukkit;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import top.mrxiaom.pluginbase.BukkitPlugin;
 import top.mrxiaom.sweet.mmorpg.api.StatType;
 import top.mrxiaom.sweet.mmorpg.comp.MMOHook;
 import top.mrxiaom.sweet.mmorpg.comp.Placeholders;
 import top.mrxiaom.sweet.mmorpg.database.PlayerDatabase;
+
+import java.util.logging.Level;
 
 public class SweetMMORPG extends BukkitPlugin {
     public static SweetMMORPG getInstance() {
@@ -47,7 +53,27 @@ public class SweetMMORPG extends BukkitPlugin {
         if (Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) {
             new Placeholders(this).register();
         }
-        MMOItems.plugin.setRPG(new MMOHook());
+        MMOHook handler = new MMOHook();
+        try {
+            // https://gitlab.com/phoenix-dvpmt/mmoitems/-/issues/1699
+            MMOItems.plugin.setRPG(handler);
+        } catch (java.lang.ClassCastException e) {
+            // 备用方案
+            RPGHandler oldRPG = MMOItems.plugin.getMainRPG();
+            if (MMOItems.plugin.isEnabled()) {
+                if (oldRPG instanceof Plugin) {
+                    HandlerList.unregisterAll((Plugin) oldRPG);
+                }
+                if (oldRPG instanceof Listener) {
+                    HandlerList.unregisterAll((Listener) oldRPG);
+                }
+            }
+            MMOItems.plugin.getRPGs().add(0, handler);
+            MMOItems.plugin.getLogger().log(Level.INFO, "Now using " + handler.getClass().getSimpleName() + " as RPG provider");
+            if (MMOItems.plugin.isEnabled()) {
+                Bukkit.getPluginManager().registerEvents(handler, this);
+            }
+        }
         Bukkit.getOnlinePlayers().forEach(playerDatabase::load);
         getLogger().info("SweetMMORPG 加载完毕");
     }
